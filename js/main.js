c34cf2e4e45c0ec101180dab84aa71a41ef153ac
mariomarin-arch <marin.mario@proton.me>
2026-07-18 21:16:34 +0100

    Create hero section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-console.log("MM Studio loaded.");
+:root {
+
+    /* =========================
+       COLORS
+    ========================== */
+
+    --color-bg: #F8F7F4;
+    --color-text: #242424;
+
+    --color-accent: #AED0D4;
+    --color-highlight: #A56A43;
+
+    /* =========================
+       TYPOGRAPHY
+    ========================== */
+
+    --font-heading: "Manrope", sans-serif;
+    --font-body: "IBM Plex Sans", sans-serif;
+
+    /* =========================
+       SPACING
+    ========================== */
+
+    --space-xs: 8px;
+    --space-s: 16px;
+    --space-m: 32px;
+    --space-l: 64px;
+    --space-xl: 128px;
+
+    /* =========================
+       BORDER RADIUS
+    ========================== */
+
+    --radius: 12px;
+
+}
